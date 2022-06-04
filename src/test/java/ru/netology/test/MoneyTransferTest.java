04959a0446f2153180d5609af0e6b731352aea76
kate <vuk_kat@mail.ru>
2022-06-04 16:34:45 +0500

    add MoneyTransferTest

--- src/test/java/ru/netology/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -1,25 +1,81 @@
 package ru.netology.test;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.DataHelper;
 import ru.netology.page.LoginPage;
 
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 
 public class MoneyTransferTest {
 
-    //тест для первого и второго варианта выглядят одинаково
-    //в Selenide инициализация полей происходит только при установке значений.
-    // Пока мы ничего не хотим установить в поля, Selenide эти поля по локаторам не ищет
-    @Test
-    void shouldTransferMoneyBetweenOwnCardsV2() {
+    @BeforeEach
+    void setup() {
         open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldTransferFromFirstCardToSecond() {
         var loginPage = new LoginPage();
-//        var LoginPage = open("http://locaihost;9999", LoginPageV2.class);
         var authInfo = DataHelper.getAuthInfo();
-        var verificationPage = loginPage.validaLogin(authInfo);
+        var verificationPage = loginPage.validLogin(authInfo);
         var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        verificationPage.validVerify(verificationCode);
+
+        var dashboardPage = verificationPage.validVerify(verificationCode);
+        var firstNumber = DataHelper.getFirstNumber();
+        var secondNumber = DataHelper.getSecondNumber();
+        int amount = 200;
+
+        var expectedBalanceFirstCard = dashboardPage.getCardBalance(firstNumber) - amount;
+        var expectedBalanceSecondCard = dashboardPage.getCardBalance(secondNumber) + amount;
+
+        var transferPage = dashboardPage.selectCardToTransfer(secondNumber);
+        dashboardPage = transferPage.makeTransfer(String.valueOf(amount), firstNumber);
+
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstNumber);
+        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondNumber);
+
+        assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
+        assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
+    }
+
+    @Test
+    void shouldTransferFromSecondCardToFirst() {
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+
+        var dashboardPage = verificationPage.validVerify(verificationCode);
+        var firstNumber = DataHelper.getSecondNumber();
+        var secondNumber = DataHelper.getFirstNumber();
+        int amount = 200;
+
+        var expectedBalanceFirstCard = dashboardPage.getCardBalance(firstNumber) - amount;
+        var expectedBalanceSecondCard = dashboardPage.getCardBalance(secondNumber) + amount;
+        var transferPage = dashboardPage.selectCardToTransfer(secondNumber);
+        dashboardPage = transferPage.makeTransfer(String.valueOf(amount), firstNumber);
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstNumber);
+        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondNumber);
+        assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
+        assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
+    }
+
+    @Test
+    void shouldWrongTransfer() {
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        var dashboardPage = verificationPage.validVerify(verificationCode);
+        var firstNumber = DataHelper.getThirdNumber();
+        var secondNumber = DataHelper.getFirstNumber();
+        int amount = 200;
+        var transferPage = dashboardPage.selectCardToTransfer(secondNumber);
+        transferPage.makeTransfer(String.valueOf(amount), firstNumber);
+        assertNull(transferPage.errorMessage());
     }
 }
